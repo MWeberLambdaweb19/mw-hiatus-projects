1671a6f1f925406a971c8ed58face06243b4d84f
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-11-06 21:42:21 -0800

    Daily commit

--- pocket_pun/front-end/pocket-pun/src/App.js
+++ pocket_pun/front-end/pocket-pun/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>This is the Pocket Punn App!</h1>
+        <p>This is a paragraph tag</p>
       </header>
     </div>
   );
